f978f73a15c1407c29478b4ad87d2c57646ab665
JovanMwesigwa <jovanmwesigwa79@gmail.com>
2023-02-26 11:17:21 +0300

    Finished the UI

--- filecon/src/components/Layout.jsx
+++ filecon/src/components/Layout.jsx
@@ -1,10 +1,18 @@
+const LayoutComponent = ({ children }) => {
+  return (
+    <div className="flex flex-1 flex-col relative bg-primaryBg w-full">
+      <nav className="w-full h-16 px-10 flex top-0 left-0 right-0 fixed bg-primaryBg shadow-sm border-b-1 border-primaryLight flex-row items-center justify-end">
+        <button className="bg-primaryLight px-4 py-2 rounded-md text-primaryText font-medium ">
+          Connect wallet
+        </button>
 
-const LayoutComponent = ()  => {
-    return (
-        <div className="flex flex-1 flex-col bg-black h-screen w-full">
-            <h1>Code</h1>
-        </div>
-    )
+        {/* <div className="bg-pink-300 px-4 py-2 rounded-md text-white font-medium ">
+          <h4>0xAddress</h4>
+        </div> */}
+      </nav>
+      <div className="mt-16">{children}</div>
+    </div>
+  )
 }
 
 export default LayoutComponent
--- filecon/src/main.jsx
+++ filecon/src/main.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import Profile from './Profile'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/profile',
+    element: <Profile />,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
